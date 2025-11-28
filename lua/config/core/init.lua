5746b575463784d226f87524a59865f1fac13b78
aghazaly <ghazaly.abdelrahman@gmail.com>
2025-11-28 15:04:07 +0200

    Update core/init.lua

--- lua/config/core/init.lua
+++ lua/config/core/init.lua
@@ -1,1 +1,19 @@
-require "config.core.options"
+local M = {}
+
+function M.setup()
+  -- set leader early so remaps can rely on it
+  vim.g.mapleader = ' '
+  vim.g.maplocalleader = ' '
+
+  -- disable some builtin providers for speed
+  vim.g.loaded_node_provider = 0
+  vim.g.loaded_perl_provider = 0
+  vim.g.loaded_ruby_provider = 0
+  vim.g.loaded_python3_provider = 0
+
+  require("config.core.options")
+  require("config.core.remaps")
+  require("config.core.custom")
+end
+
+return M
